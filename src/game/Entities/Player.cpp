d750542bb9e413378fc2284ab74155cb4b4d4055
celguar <celguar@gmail.com>
2022-09-04 02:49:36 +0300

    Learn all my level command

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -19533,7 +19533,6 @@ void Player::learnClassLevelSpells(bool includeHighLevelQuestRewards)
     ChrClassesEntry const* clsEntry = sChrClassesStore.LookupEntry(getClass());
     if (!clsEntry)
         return;
-
     uint32 family = clsEntry->spellfamily;
 
     // special cases which aren't sourced from trainers and normally require quests to obtain - added here for convenience
