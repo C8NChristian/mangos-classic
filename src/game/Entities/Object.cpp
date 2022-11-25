9ef9c2b953cfc4aa4c1da6656535d139a863d21f
celguar <celguar@gmail.com>
2022-11-25 04:55:05 +0300

    Lower the amount of packets/work being done for bots

--- src/game/Entities/Object.cpp
+++ src/game/Entities/Object.cpp
@@ -2516,6 +2516,7 @@ struct WorldObjectChangeAccumulator
             {
 #endif
             if (owner != &i_object && owner->HasAtClient(&i_object))
+#endif
                 i_object.BuildUpdateDataForPlayer(owner, i_updateDatas);
 #ifdef ENABLE_PLAYERBOTS
             }
