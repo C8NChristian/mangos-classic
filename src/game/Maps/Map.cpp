cf5f06cf320e78feb4e9b88b4d229f7a25d7de12
celguar <celguar@gmail.com>
2024-03-26 20:59:02 +0300

    Fix merge

--- src/game/Maps/Map.cpp
+++ src/game/Maps/Map.cpp
@@ -1039,7 +1039,6 @@ void Map::Update(const uint32& t_diff)
             }
         }
     }
-#endif
 
     // Reset the has real players flag and check for it again
     const bool hadRealPlayers = hasRealPlayers;
@@ -1098,7 +1097,7 @@ void Map::Update(const uint32& t_diff)
                     {
                         shouldUpdateBot = true;
                     }
-                }*/
+                }
             }
 
             // Save the active characters for later logs
@@ -1177,7 +1176,6 @@ void Map::Update(const uint32& t_diff)
     }
 
     const bool shouldUpdateObjects = urand(0, (uint32)(objectUpdateChance * 100)) < 100;
-#endif
 
     // non-player active objects
     bool updateObj = urand(0, (HasRealPlayers() ? avgDiff : (avgDiff * 3))) < 10;
